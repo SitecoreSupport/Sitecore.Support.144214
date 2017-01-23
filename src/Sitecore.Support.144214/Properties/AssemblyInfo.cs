d6d1c418dd1e6196e3f4dcb9690b6b719b528638
Alexey Lunev <alu@sitecore.net>
2017-01-23 16:17:03 +0200

    Initial patch commit

---
+++ src/Sitecore.Support.144214/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.144214")]
+[assembly: AssemblyProduct("Sitecore.Support.144214")]
+[assembly: ComVisible(false)]
---
+++ src/Sitecore.Support.144214/Sitecore/Support/FixLayoutDeltaInExperienceEditor.cs
@@ -0,0 +1,37 @@
+﻿using Sitecore;
+using Sitecore.Data;
+using Sitecore.Data.Fields;
+using Sitecore.Data.Items;
+using Sitecore.Pipelines.Save;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.Support
+{
+  public class FixLayoutDeltaInExperienceEditor
+  {
+    // Fields
+    private static ID Layout = new ID("{E1D68787-D22B-4EA2-82B3-84C282E375EB}");
+
+    // Methods
+    public void Process(SaveArgs args)
+    {
+      if (Sitecore.Context.PageMode.IsExperienceEditor)
+      {
+        foreach (Item item in args.SavedItems)
+        {
+          Field field = item.Fields[Layout];
+          LayoutField field2 = item.Fields[FieldIDs.LayoutField];
+          if (((field != null) && (field2 != null)) && !string.IsNullOrEmpty(field2.Value))
+          {
+            item.Editing.BeginEdit();
+            LayoutField.SetFieldValue(field2.InnerField, field2.Value);
+            item.Editing.EndEdit();
+          }
+        }
+      }
+    }
+  }
+}
